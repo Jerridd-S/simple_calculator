b2cf98bdf5568c31c456226f70ad89314e2e9a68
Jerridd-S <105670541+Jerridd-S@users.noreply.github.com>
2022-12-06 22:55:06 -0600

    React with Bootstrap created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Simple Calculator!</h1>
+      <button type="button" class="btn btn-outline-dark">
+        Dark
+      </button>
+      <a class="btn btn-primary" href="#" role="button">
+        Link
+      </a>
     </div>
   );
-}
+};
 
 export default App;
