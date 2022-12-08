d9beb9a338e943cce860e6b113f3f970f3f78ec9
Jerridd-S <105670541+Jerridd-S@users.noreply.github.com>
2022-12-07 21:47:15 -0600

    added buttons and display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,94 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const Display = () => {
+  return (
+    <div>
+      <input type="text" readOnly></input>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
-      <h1>Simple Calculator!</h1>
-      <button type="button" class="btn btn-outline-dark">
-        Dark
-      </button>
-      <a class="btn btn-primary" href="#" role="button">
-        Link
-      </a>
+      <div>
+        <Display></Display>
+      </div>
+      <div>
+        <button type="button" class="btn btn-outline-dark">
+          C
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          ()
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          %
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          /
+        </button>
+      </div>
+
+      <div>
+        <button type="button" class="btn btn-outline-dark">
+          7
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          8
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          9
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          x
+        </button>
+      </div>
+
+      <div>
+        <button type="button" class="btn btn-outline-dark">
+          4
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          5
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          6
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          -
+        </button>
+      </div>
+
+      <div>
+        <button type="button" class="btn btn-outline-dark">
+          1
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          2
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          3
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          +
+        </button>
+      </div>
+
+      <div>
+        <button type="button" class="btn btn-outline-dark">
+          +/-
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          0
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          .
+        </button>
+        <button type="button" class="btn btn-outline-dark">
+          =
+        </button>
+      </div>
     </div>
   );
 };
