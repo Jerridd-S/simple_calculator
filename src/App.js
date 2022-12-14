699060de3c55c3c9e615d06e6c02f8e312bf5845
Jerridd-S <105670541+Jerridd-S@users.noreply.github.com>
2022-12-13 22:16:39 -0600

    Added functionality of all buttons

--- src/App.js
+++ src/App.js
@@ -1,91 +1,233 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-const Display = () => {
-  return (
-    <div>
-      <input type="text" readOnly></input>
-    </div>
-  );
-};
+import { useState } from "react";
 
 const App = () => {
+  const [val, setVal] = useState("");
+
+  const clearClicked = () => {
+    setVal("");
+  };
+  const zeroClicked = () => {
+    setVal(val + "0");
+  };
+  const oneClicked = () => {
+    setVal(val + "1");
+  };
+  const twoClicked = () => {
+    setVal(val + "2");
+  };
+  const threeClicked = () => {
+    setVal(val + "3");
+  };
+  const fourClicked = () => {
+    setVal(val + "4");
+  };
+  const fiveClicked = () => {
+    setVal(val + "5");
+  };
+  const sixClicked = () => {
+    setVal(val + "6");
+  };
+  const sevenClicked = () => {
+    setVal(val + "7");
+  };
+  const eightClicked = () => {
+    setVal(val + "8");
+  };
+  const nineClicked = () => {
+    setVal(val + "9");
+  };
+  const mutiplyClicked = () => {
+    setVal(val + "*");
+  };
+  const divideClicked = () => {
+    setVal(val + "/");
+  };
+  const plusClicked = () => {
+    setVal(val + "+");
+  };
+  const subtactClicked = () => {
+    setVal(val + "-");
+  };
+  const percentageClicked = () => {
+    setVal(val + "%");
+  };
+  const decimalClicked = () => {
+    setVal(val + ".");
+  };
+  const equalsClicked = () => {
+    setVal(eval(val));
+  };
+  const negateClicked = () => {
+    setVal(-val);
+  };
+  const leftParenClicked = () => {
+    setVal(val + "(");
+  };
+  const rightParenClicked = () => {
+    setVal(val + ")");
+  };
+  const backspaceClicked = () => {
+    setVal(val.slice(0, -1));
+  };
   return (
     <div>
       <div>
-        <Display></Display>
+        <input value={val}></input>
       </div>
       <div>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={backspaceClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
+          del
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={clearClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           C
         </button>
-        <button type="button" class="btn btn-outline-dark">
-          ()
+        <button
+          onClick={leftParenClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
+          (
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={rightParenClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
+          )
+        </button>
+        <button
+          onClick={percentageClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           %
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={divideClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           /
         </button>
       </div>
-
       <div>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={sevenClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           7
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={eightClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           8
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={nineClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           9
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={mutiplyClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           x
         </button>
       </div>
-
       <div>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={fourClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           4
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={fiveClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           5
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button onClick={sixClicked} type="button" class="btn btn-outline-dark">
           6
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={subtactClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           -
         </button>
       </div>
-
       <div>
-        <button type="button" class="btn btn-outline-dark">
+        <button onClick={oneClicked} type="button" class="btn btn-outline-dark">
           1
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button onClick={twoClicked} type="button" class="btn btn-outline-dark">
           2
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={threeClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           3
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={plusClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           +
         </button>
       </div>
 
       <div>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={negateClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           +/-
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={zeroClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           0
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={decimalClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           .
         </button>
-        <button type="button" class="btn btn-outline-dark">
+        <button
+          onClick={equalsClicked}
+          type="button"
+          class="btn btn-outline-dark"
+        >
           =
         </button>
       </div>
